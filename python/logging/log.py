209c86d67a080d1b6fd9441c5c19069612195551
Sandeep Singh Rawat <sandeepr@one.com>
2019-07-28 02:09:13 +0530

    added logging and memory docs and program

---
+++ python/logging/log.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import logging
+
+
+
+logging.basicConfig(filename='program.log',level=logging.DEBUG)
+logging.warning('An example message.')
+logging.warning('Another message')
